142ca1d775d545d89e407db6fb338a25c01cef27
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-09-09 13:35:48 +0200

    link ka shopu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import videocoffee from '../videos/videocoffee.mp4'
+import { Link } from "react-router-dom";
 
 export const Main = () => {
   return (
     <div className='main'>
         <video src={videocoffee} autoPlay muted loop />
         <div className='content'>
-            <h1>LETS DRINK COFFEE</h1>
-            <h3>SHOP</h3>
+            <h1>LET'S DRINK COFFEE</h1>
+             <Link to="/products"><h3 style={{color:"wheat"}}>SHOP</h3></Link>
         </div>
     </div>
   )
